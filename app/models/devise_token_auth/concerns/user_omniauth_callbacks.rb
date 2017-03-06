86fb079c21de67df0ee64f64124c212761fca43c
Yoonji Jang <yjang@Yoonjis-Macbook-Pro.local>
2017-03-06 19:03:20 +0900

    Move generate_uid

--- app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
+++ app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
@@ -10,26 +10,6 @@ module DeviseTokenAuth::Concerns::UserOmniauthCallbacks
     # only validate unique uids among email registration users
     validate :unique_uid_user, on: :create
     validate :unique_email_user, on: :create
-
-    protected
-
-    # only validate unique email among users that registered by email
-    def unique_uid_user
-      if provider == 'email' && self.class.where(provider: 'email', uid: uid).count > 0
-        errors.add(:uid, :taken)
-      end
-    end
-
-    def unique_email_user
-      if provider == 'email' && self.class.where(provider: 'email', email: email).count > 0
-        errors.add(:email, :taken)
-      end
-    end
-
-    private
-    def generate_uid
-      self.uid = self.class.uid unless self.uid.present?
-    end
   end
 
   module ClassMethods
@@ -39,5 +19,25 @@ module DeviseTokenAuth::Concerns::UserOmniauthCallbacks
         break token unless to_adapter.find_first({uid: token})
       end
     end
+  end
+
+  protected
+
+  # only validate unique email among users that registered by email
+  def unique_uid_user
+    if provider == 'email' && self.class.where(provider: 'email', uid: uid).count > 0
+      errors.add(:uid, :taken)
+    end
+  end
+
+  def unique_email_user
+    if provider == 'email' && self.class.where(provider: 'email', email: email).count > 0
+      errors.add(:email, :taken)
+    end
+  end
+
+  private
+  def generate_uid
+    self.uid = self.class.uid unless self.uid.present?
   end
 end
